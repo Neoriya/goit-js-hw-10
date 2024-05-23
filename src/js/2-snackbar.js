f4d1cc21209cd37749a3c9344013837f1c11fb8a
Neoriya <alena.neori@gmail.com>
2024-05-23 21:21:36 +0300

    intermediate version "in the process of execution"

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,18 @@
-console.log('snackbar');
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+// const inputWindow = document.querySelector('button[data-start]');
+// const button = document.querySelector('#datetime-picker');
+
+const prom = new Promise((reslove, reject) => {
+  const random = Math.random();
+
+  setTimeout(() => {
+    if (random > 0.5) {
+      reslove('fulfilled');
+    } else {
+      reject('rejected');
+    }
+  }, 1000);
+});
+console.log(prom);
