004d49f16149bc651a4162bf44a144e3255c02e5
Neoriya <100101631+Neoriya@users.noreply.github.com>
2024-05-23 18:21:53 +0000

    Deploying to gh-pages from @ Neoriya/goit-js-hw-10@f4d1cc21209cd37749a3c9344013837f1c11fb8a 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/reset-df635d97.js";/* empty css                      */console.log("snackbar");
+import"./assets/reset-df635d97.js";/* empty css                      */import"./assets/vendor-77e16229.js";const m=new Promise((o,e)=>{const t=Math.random();setTimeout(()=>{t>.5?o("fulfilled"):e("rejected")},1e3)});console.log(m);
 //# sourceMappingURL=commonHelpers2.js.map
